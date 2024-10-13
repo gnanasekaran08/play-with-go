c11e92ba16b3a6149e03bd37ffd194e88e0847ea
Gnanasekaran <37533175+gnanasekaran08@users.noreply.github.com>
2024-10-14 01:05:45 +0530

    write logs

--- time.go
+++ time.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"time"
 )
 
@@ -9,7 +11,12 @@ func main() {
 	fmt.Println("---> Play With Time <---")
 	// location, _ := time.LoadLocation("Asia/Kolkata")
 	// timeLayout := "2022-01-01 15:04:05"
+	// read only date and time
 
+	file, _ := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	log.SetOutput(file)
+
+	log.Println("Current Time :", time.Now().Local().Format("2006-01-02 15:04:05"))
 	now := time.Now()
 	fmt.Println("Current Time :", now)
 	timer := "23:12"
