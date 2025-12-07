92614d0bb189ddf1cfadb5bbab07be9ae8b4bdd4
Gnanasekaran <37533175+gnanasekaran08@users.noreply.github.com>
2025-12-07 17:12:07 +0530

    connect db

--- db/db.go
+++ db/db.go
@@ -1,7 +1,34 @@
 package db
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
 
 func Connect() {
 	fmt.Println("Connecting to the database...")
+	config := mysql.NewConfig()
+	config.User = "root"
+	config.Passwd = ""
+	config.Net = "tcp"
+	config.Addr = "127.0.0.1:3306"
+	config.DBName = "reallist_one"
+
+	db, err := sql.Open("mysql", config.FormatDSN())
+	if err != nil {
+		fmt.Println("Error connecting to the database:", err)
+		return
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Database ping failed:", err)
+		return
+	}
+
+	fmt.Println("Database connection established successfully!")
 }
