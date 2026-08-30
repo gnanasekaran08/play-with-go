41843cc3c98a670d572eb9e826a14abb1a4561c5
Gnanasekaran <37533175+gnanasekaran08@users.noreply.github.com>
2026-08-30 15:59:36 +0530

    tiny changes

---
+++ sqlc/migrations/000001_create_todos_table.down.sql
@@ -0,0 +1,8 @@
+CREATE TABLE
+    todos (
+        id SERIAL PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT,
+        completed BOOLEAN DEFAULT FALSE,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
